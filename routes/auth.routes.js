96a477418c221d9d08d5749c8af9e4da14ad7b80
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2020-08-17 22:59:11 -0500

    2020-08-17

---
+++ routes/auth.routes.js
@@ -0,0 +1,74 @@
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const router = Router();
+
+// /api/auth/register
+router.post(
+  "/register",
+  [
+    check("email", "bad email").isEmail(),
+    check("password", "Password should be at list 6 symbols.").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "wrong registration data",
+        });
+      }
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        res.status(400).json({ message: "User exist." });
+      }
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+      await user.save();
+      res.status(201).json({ message: "User created!" });
+    } catch (e) {
+      return res.status(500).json({ message: "Something wrong, try again!" });
+    }
+  }
+);
+
+// /api/auth/login
+router.post(
+  "/login",
+  [
+    check("email", "Enter email").normalizeEmail().isEmail(),
+    check("password", "Enter password").exists(),
+  ],
+
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "wrong login data",
+        });
+      }
+      //
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+
+      if(!user){
+          return res.status(400).json({message: "User is not exist"})
+      }
+
+      //
+    } catch (e) {
+      return res.status(500).json({ message: "Something wrong, try again!" });
+    }
+  }
+);
+
+module.exports = router;
