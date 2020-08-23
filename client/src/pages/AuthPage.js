ecf252a5a24edc1667bc129a4eb061f622544cc1
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2020-08-22 22:17:06 -0500

    2020-08-22

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { useState } from "react";
+import { useHttp } from "../hooks/http.hook";
 export const AuthPage = () => {
+  //const { loading, error, request } = useHttp();
+  const { loading, error, request } = useHttp();
+  const [form, setform] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (event) => {
+    setform({ ...form, [event.target.name]: event.target.value });
+  };
+  const registerHandler = async () => {
+    try {
+      const data = await request("/api/auth/register", "POST", { ...form });
+      console.log("Data", data);
+    } catch (e) {}
+  };
+
   return (
     <div className="row">
       <div className="col s12 m6">
@@ -7,12 +26,44 @@ export const AuthPage = () => {
         <div className="card blue darken-1">
           <div className="card-content white-text">
             <span className="card-title">Authorization</span>
-            <div></div>
+            <div className="input-field">
+              <input
+                placeholder="Enter your email"
+                id="email"
+                type="text"
+                name="email"
+                className="yellow-input"
+                onChange={changeHandler}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="input-field">
+              <input
+                placeholder="Enter password"
+                id="password"
+                type="password"
+                name="password"
+                className="yellow-input"
+                onChange={changeHandler}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
           </div>
           <div className="card-action">
-            <button className="btn yellow darken-4" style={{marginRight:10}}>Login</button>
-            <button className="btn grey lighten-1 black-text">Register</button>
-
+            <button
+              className="btn yellow darken-4"
+              style={{ marginRight: 10 }}
+              disabled={loading}
+            >
+              Login
+            </button>
+            <button
+              className="btn grey lighten-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              Register
+            </button>
           </div>
         </div>
       </div>
